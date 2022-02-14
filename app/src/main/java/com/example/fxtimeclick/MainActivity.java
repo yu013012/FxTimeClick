4ad2f5e6e28fcb4bfac61137d8dfc772c12196c0
yu013012 <youdanyu943@gmail.com>
2022-02-14 12:09:38 +0900

    buttonの追加

--- app/src/main/java/com/example/fxtimeclick/MainActivity.java
+++ app/src/main/java/com/example/fxtimeclick/MainActivity.java
@@ -21,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
         TextView text = findViewById(R.id.textView);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +40,148 @@ public class MainActivity extends AppCompatActivity {
                 // 日時情報を指定フォーマットの文字列で取得
                 String java8Disp = nowDateTime.format(java8Format);
                 text.setText(java8Disp);
-                url = url + java8Disp;
+                String parm = ",'1','50/100'";
+                url = url + "'" + java8Disp + "'" + parm;
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                java8Disp = "";
+                url = "http://yniwa0130.php.xdomain.jp/index.php?a=";
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                // 現在日時情報で初期化されたインスタンスの取得
+                LocalDateTime nowDateTime = LocalDateTime.now();
+                DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+                // 日時情報を指定フォーマットの文字列で取得
+                String java8Disp = nowDateTime.format(java8Format);
+                text.setText(java8Disp);
+                String parm = ",'1','25/100'";
+                url = url + "'" + java8Disp + "'" + parm;
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                java8Disp = "";
+                url = "http://yniwa0130.php.xdomain.jp/index.php?a=";
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                // 現在日時情報で初期化されたインスタンスの取得
+                LocalDateTime nowDateTime = LocalDateTime.now();
+                DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+                // 日時情報を指定フォーマットの文字列で取得
+                String java8Disp = nowDateTime.format(java8Format);
+                text.setText(java8Disp);
+                String parm = ",'1','50/50'";
+                url = url + "'" + java8Disp + "'" + parm;
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                java8Disp = "";
+                url = "http://yniwa0130.php.xdomain.jp/index.php?a=";
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                // 現在日時情報で初期化されたインスタンスの取得
+                LocalDateTime nowDateTime = LocalDateTime.now();
+                DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+                // 日時情報を指定フォーマットの文字列で取得
+                String java8Disp = nowDateTime.format(java8Format);
+                text.setText(java8Disp);
+                String parm = ",'1','25/50'";
+                url = url + "'" + java8Disp + "'" + parm;
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                java8Disp = "";
+                url = "http://yniwa0130.php.xdomain.jp/index.php?a=";
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                // 現在日時情報で初期化されたインスタンスの取得
+                LocalDateTime nowDateTime = LocalDateTime.now();
+                DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+                // 日時情報を指定フォーマットの文字列で取得
+                String java8Disp = nowDateTime.format(java8Format);
+                text.setText(java8Disp);
+                String parm = ",'2','50/100'";
+                url = url + "'" + java8Disp + "'" + parm;
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                java8Disp = "";
+                url = "http://yniwa0130.php.xdomain.jp/index.php?a=";
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                // 現在日時情報で初期化されたインスタンスの取得
+                LocalDateTime nowDateTime = LocalDateTime.now();
+                DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+                // 日時情報を指定フォーマットの文字列で取得
+                String java8Disp = nowDateTime.format(java8Format);
+                text.setText(java8Disp);
+                String parm = ",'2','25/100'";
+                url = url + "'" + java8Disp + "'" + parm;
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                java8Disp = "";
+                url = "http://yniwa0130.php.xdomain.jp/index.php?a=";
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                // 現在日時情報で初期化されたインスタンスの取得
+                LocalDateTime nowDateTime = LocalDateTime.now();
+                DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+                // 日時情報を指定フォーマットの文字列で取得
+                String java8Disp = nowDateTime.format(java8Format);
+                text.setText(java8Disp);
+                String parm = ",'2','50/50'";
+                url = url + "'" + java8Disp + "'" + parm;
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                java8Disp = "";
+                url = "http://yniwa0130.php.xdomain.jp/index.php?a=";
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                // 現在日時情報で初期化されたインスタンスの取得
+                LocalDateTime nowDateTime = LocalDateTime.now();
+                DateTimeFormatter java8Format = DateTimeFormatter.ofPattern("MM/dd HH:mm:ss");
+                // 日時情報を指定フォーマットの文字列で取得
+                String java8Disp = nowDateTime.format(java8Format);
+                text.setText(java8Disp);
+                String parm = ",'2','25/50'";
+                url = url + "'" + java8Disp + "'" + parm;
                 Uri uri = Uri.parse(url);
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
